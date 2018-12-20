e7782dbb13f4b0eddde5f0f1609d42d7af946383
Daniel Herron <heatwaveqc@yahoo.com>
2018-12-20 13:32:04 -0600

    Lib class update

--- CharacterSheet.API/ClassLibrary/Character.cs
+++ CharacterSheet.API/ClassLibrary/Character.cs
@@ -9,5 +9,12 @@ namespace ClassLibrary
         public int charID { get; set; }                     //character ID
         public int userID { get; set; }                     //ID of user the character belongs to
         public int CampID { get; set; }                     //ID of campaign the character belongs to
+
+        public Dictionary<string, int> SkillList { get; set; }
+        public Dictionary<string, int> FeatList { get; set; }
+        public Dictionary<string, int> Invantory { get; set; }
+        public Dictionary<string, int[]> SpellSlots { get; set; }
+        public List<Spell> SpellsKnown { get; set; }
+
     }
 }
