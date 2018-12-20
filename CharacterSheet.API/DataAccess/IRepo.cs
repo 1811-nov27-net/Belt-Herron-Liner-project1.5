24cfbad2f9e3931f44107e8c4dc6126dea96642c
Matthew Liner <mattliner028@gmail.com>
2018-12-20 13:39:56 -0600

    Repo interface created. CreateCharacter and CharacterList functions declared (with no args, yet)

---
+++ CharacterSheet.API/DataAccess/IRepo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess
+{
+    interface IRepo
+    {
+        void CreateCharacter(/*args*/);                     //create a new character
+        IEnumerable CharacterList<Character>(/*args*/);     //grab a character list
+
+    }
+}
