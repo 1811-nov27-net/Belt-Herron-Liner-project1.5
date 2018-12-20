6fb870854f63acc563058bee5b9ff27dba78849d
willbelt <willbeltit@gmail.com>
2018-12-20 13:06:39 -0600

    Added ChracterSheet.MVC WebApp Solution

---
+++ CharacterSheet.MVC/CharacterSheet.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CharacterSheet.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
