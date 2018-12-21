df260748438bc049afde19168727b060d1a519f6
Daniel Herron <heatwaveqc@yahoo.com>
2018-12-21 14:14:53 -0600

    Some changes to classes.

--- CharacterSheet.API/ClassLibrary/Campaign.cs
+++ CharacterSheet.API/ClassLibrary/Campaign.cs
@@ -7,6 +7,7 @@ namespace ClassLibrary
     public class Campaign
     {
         public int CampID { get; set; }                 //campaign ID
+        public string Name { get; set; }
 
         public List<Character> Characters = new List<Character>();
         public List<User> GMs = new List<User>();
--- D20DatabaseCreation.sql
+++ D20DatabaseCreation.sql
@@ -2,3 +2,25 @@ create schema d20
 
 go
 
+create table d20.Gamer
+(
+	GamerId int identity not null,
+	UserName nvarchar(100) not null unique,
+	constraint d20_Gamer_ID primary key (GamerId)
+);
+
+create table d20.Campaign
+(
+	CampaignId int identity not null,
+	CampaignName nvarchar(100) not null unique,
+	constraint d20_Campaign_ID primary key (CampaignId)
+);
+
+create table d20.GMJunction
+(
+	CampaignId int not null,
+	GMId int not null,
+	constraint FK_GM_Gamer Foreign key (GMId) references d20.Gamer (GamerId),
+	constraint FK_GM_Campaign Foreign key (CampaignId) references d20.Campaign (CampaignId)
+
+);
