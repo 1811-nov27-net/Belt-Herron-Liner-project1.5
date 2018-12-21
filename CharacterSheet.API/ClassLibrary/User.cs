d8425d8dfed52bf3f59e2bb4db07739a49413005
Daniel Herron <heatwaveqc@yahoo.com>
2018-12-21 14:18:14 -0600

    Database Branch Created.

--- CharacterSheet.API/ClassLibrary/User.cs
+++ CharacterSheet.API/ClassLibrary/User.cs
@@ -5,8 +5,8 @@ namespace ClassLibrary
 {
     public class User
     {
-        public string username { get; set; }                //username
-        public int userID { get; set; }                     //user ID
+        public string Username { get; set; }                //username
+        public int UserID { get; set; }                     //user ID
 
         public List<Character> Characters = new List<Character>();
         public List<Campaign> MyCampaigns = new List<Campaign>();
