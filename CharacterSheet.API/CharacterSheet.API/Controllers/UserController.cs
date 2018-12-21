3d6c8873469af52a1d612f6ea2eb4efdc102eb05
willbelt <willbeltit@gmail.com>
2018-12-21 16:00:34 -0600

    Wrote User Login, Registration, Logout, in User Controller, Authorization Configuration in Startup.cs for .API solution

--- CharacterSheet.API/CharacterSheet.API/Controllers/UserController.cs
+++ CharacterSheet.API/CharacterSheet.API/Controllers/UserController.cs
@@ -2,9 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ClassLibrary;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
+using CharacterSheet.API.Models;
 
 namespace CharacterSheet.API.Controllers
 {
@@ -13,7 +17,77 @@ namespace CharacterSheet.API.Controllers
     [Authorize]
     public class UserController : Controller
     {
+        private SignInManager<IdentityUser> _SignInManager;
 
+        public UserController (SignInManager<IdentityUser> signInManager, IdentityDbContext db)
+        {
+            db.Database.EnsureCreated();
+            _SignInManager = signInManager;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Login(UserViewModel user)
+        {
+            var result = await _SignInManager.PasswordSignInAsync(user.UserName, user.Password, isPersistent: false, lockoutOnFailure: false);
+            if (!result.Succeeded)
+            {
+                return StatusCode(403);
+            }
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Register(UserViewModel user, 
+            [FromServices]UserManager<IdentityUser> userManager, 
+            [FromServices] RoleManager<IdentityRole> roleManager, bool GM = false)
+        {
+            var newUser = new IdentityUser(user.UserName);
+
+            var result = await userManager.CreateAsync(newUser, user.Password);
+
+            if(!result.Succeeded)
+            {
+                return BadRequest(result);
+            }
+
+            if (GM)
+            {
+                var result2 = await roleManager.RoleExistsAsync("GM");
+                if(!result2)
+                {
+                    var GMRole = new IdentityRole("GM");
+                    result = await roleManager.CreateAsync(GMRole);
+
+                    if(!result.Succeeded)
+                    {
+                        return StatusCode(500, result);
+                    }
+                    result = await userManager.AddToRoleAsync(newUser, "GM");
+                    if(!result.Succeeded)
+                    {
+                        return StatusCode(500, result);
+                    }
+                }
+            }
+            await _SignInManager.SignInAsync(newUser, isPersistent: false);
+
+            return NoContent();
+        }
+
+        public async Task<NoContentResult> Logout()
+        {
+            await _SignInManager.SignOutAsync();
+
+            return NoContent();
+        }
+
+        [HttpGet]
+        [Authorize]
+        public string LoggedInUser()
+        {
+            var roles = User.IsInRole("GM");
+            return User.Identity.Name;
+        }
 
         // GET: User
         public ActionResult Index()
--- CharacterSheet.API/CharacterSheet.API/Startup.cs
+++ CharacterSheet.API/CharacterSheet.API/Startup.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -26,6 +30,36 @@ namespace CharacterSheet.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddDbContext<IdentityDbContext>(options => 
+                options.UseSqlServer(Configuration.GetConnectionString("UserApiAuthorization")));
+
+            services.AddIdentity<IdentityUser, IdentityRole>(options =>
+                {
+                    options.Password.RequiredLength = 25;
+                    options.Password.RequiredUniqueChars = 1;
+                }).AddEntityFrameworkStores<IdentityDbContext>();
+
+            services.ConfigureApplicationCookie(options =>
+                {
+                    options.Cookie.Name = "UserApiAuthorization";
+                    options.ExpireTimeSpan = TimeSpan.FromMinutes(120);
+                    options.Events = new CookieAuthenticationEvents
+                    {
+                        OnRedirectToLogin = ctx =>
+                        {
+                            ctx.Response.StatusCode = 401;
+                            return Task.FromResult(0);
+                        },
+                        OnRedirectToAccessDenied = ctx =>
+                        {
+                            ctx.Response.StatusCode = 403;
+                            return Task.FromResult(0);
+                        }
+                    };
+                });
+
+            services.AddAuthentication();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,7 +74,7 @@ namespace CharacterSheet.API
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+            app.UseAuthentication();
             app.UseHttpsRedirection();
             app.UseMvc();
         }
