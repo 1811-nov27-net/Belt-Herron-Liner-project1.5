191007631b85fa756bf45cb8c446ab949d0446fd
Matthew Liner <mattliner028@gmail.com>
2018-12-21 15:51:57 -0600

    CampaignController CampList (no error, but untested)

--- CharacterSheet.API/CharacterSheet.API/Controllers/CampaignController.cs
+++ CharacterSheet.API/CharacterSheet.API/Controllers/CampaignController.cs
@@ -5,90 +5,34 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using ClassLibrary;
+using DataAccess;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Primitives;
+using System.Net.Http;
+using System.Text;
+
 
 namespace CharacterSheet.API.Controllers
 {
-    public class CampaignController : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class CampaignController : ControllerBase
     {
-        // GET: Campaign
-        public ActionResult Index()
+
+        public IRepo Repo { get; set; }
+
+        public CampaignController(IRepo repo)
         {
-            return View();
+            Repo = repo;
         }
 
-        // GET: Campaign/Details/5
-        public ActionResult Details(int id)
+        [HttpGet]
+        public ActionResult<IEnumerable<Campaign>> Get()
         {
-            return View();
+            var data = Repo.CampList().ToList();
+            return data;
         }
 
-        // GET: Campaign/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Campaign/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Campaign/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: Campaign/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Campaign/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: Campaign/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
     }
 }
