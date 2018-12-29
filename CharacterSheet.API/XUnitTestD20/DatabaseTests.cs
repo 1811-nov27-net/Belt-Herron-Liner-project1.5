9e0d393dfbfd2ddf2f56975717325242cc13ecde
willbelt <willbeltit@gmail.com>
2018-12-28 22:38:47 -0600

    Wrote CampList Test

--- CharacterSheet.API/XUnitTestD20/DatabaseTests.cs
+++ CharacterSheet.API/XUnitTestD20/DatabaseTests.cs
@@ -366,5 +366,69 @@ namespace XUnitTestD20
                 Assert.True(actual);
             }
         }
+    [Fact]
+    public async void AddGMWorks()
+        {
+            var options = new DbContextOptionsBuilder<Data.D20CharacterDatabaseContext>()
+                .UseInMemoryDatabase("add_gm_test").Options;
+            using (var db = new Data.D20CharacterDatabaseContext(options))
+            {
+                Data.IRepo sut = new Data.D20Repo(db);
+                Lib.User user = new Lib.User();
+                user.UserID = 0;
+                user.Username = "Test User";
+                user.Characters = new List<Lib.Character>();
+                user.MyCampaigns = new List<Lib.Campaign>();
+                sut.CreateUser(user);
+                Lib.Campaign camp = new Lib.Campaign();
+                camp.CampID = 0;
+                camp.Name = "Test Campaign";
+                camp.Characters = new List<Lib.Character>();
+                camp.GMs = new List<Lib.User>();
+                sut.CreateCampaign(camp);
+                user = await db.Gamer.FirstOrDefaultAsync(u => u.UserName == "Test User");
+                camp = await db.Campaign.FirstOrDefaultAsync(c => c.CampaignName == "Test Campaign");
+
+                sut.AddGM(camp.CampID, user.UserID);
+
+                var result = await db.Gmjunction.FirstOrDefaultAsync(g => g.CampaignId == camp.CampID);
+                bool actual = result.Gmid == user.UserID;
+
+                Assert.True(actual);
+            }
+        }
+        [Fact]
+        public async void CampaignListWorks()
+        {
+            var options = new DbContextOptionsBuilder<Data.D20CharacterDatabaseContext>()
+                .UseInMemoryDatabase("campaign_list_test").Options;
+            using (var db = new Data.D20CharacterDatabaseContext(options))
+            {
+                Data.IRepo sut = new Data.D20Repo(db);
+                Lib.Campaign camp = new Lib.Campaign();
+                camp.CampID = 0;
+                camp.Name = "Test Campaign";
+                camp.Characters = new List<Lib.Character>();
+                camp.GMs = new List<Lib.User>();
+                sut.CreateCampaign(camp);
+                Lib.Campaign camp2 = new Lib.Campaign();
+                camp2.CampID = 0;
+                camp2.Name = "Test Campaign2";
+                camp2.Characters = new List<Lib.Character>();
+                camp2.GMs = new List<Lib.User>();
+                sut.CreateCampaign(camp2);
+
+                Data.Campaign testCamp = await db.Campaign.FirstOrDefaultAsync(c => c.CampaignName == camp.Name);
+                Data.Campaign testCamp2 = await db.Campaign.FirstOrDefaultAsync(c => c.CampaignName == camp2.Name);
+                bool actual = (testCamp != null && testCamp.CampaignName == camp.Name && testCamp2 != null && testCamp2.CampaignName == camp2.Name);
+                Assert.True(actual);
+
+                IList<Lib.Campaign> campList = (List<Lib.Campaign>)sut.CampList();
+
+                actual = (campList.Count == 2 && campList[0].Name == camp.Name && campList[1].Name == camp2.Name);
+
+                Assert.True(actual);
+            }
+        }
     }
 }
